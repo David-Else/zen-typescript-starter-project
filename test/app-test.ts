b0c024c7805c5fe39d1519305544e69c42908fc4
David <12832280+David-Else@users.noreply.github.com>
2019-04-16 17:27:42 +0100

    Update config

--- test/app-test.ts
+++ test/app-test.ts
@@ -1,9 +1,9 @@
 import { strict as assert } from 'assert';
 import { test } from '../src/app.js';
 
-describe('Example test', () => {
-  describe('test()', () => {
-    it('should return 3 when it has a passed parameter of 2', () => {
+describe('Example test', (): void => {
+  describe('test()', (): void => {
+    it('should return 3 when it has a passed parameter of 2', (): void => {
       // Arrange
       const testData = 2;
       // Act

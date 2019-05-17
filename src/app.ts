5d3a01eb801ee759b671e2d086b0f419ea9210f4
David <12832280+David-Else@users.noreply.github.com>
2019-05-17 15:30:12 +0100

    Update for Quokka and a few little changes

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
+// Enable the Quokka.js jsdom plugin and parse index.html
+
+// eslint-disable-next-line no-unused-expressions
+// ({
+//   plugins: ['jsdom-quokka-plugin'],
+//   jsdom: { file: './index.html' },
+// });
+
 export function test(x: number): number {
   return x + 1;
 }
